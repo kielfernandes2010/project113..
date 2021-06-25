572cc3d7bb3dfdb17b261f0f80af2e94668ae474
kielfernandes2010 <74675067+kielfernandes2010@users.noreply.github.com>
2021-06-25 16:21:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position (150,150);
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+}
+
+
+ 
+
+function draw() {
+    image(video, 230, 150, 220, 200);
+
+    fill(255, 0, 0 );
+    stroke(255, 0, 0);
+    circle(50, 50, 80);
+
+
+    fill(0, 128, 0 );
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20)
+
+rect(90, 420, 460, 20)
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
